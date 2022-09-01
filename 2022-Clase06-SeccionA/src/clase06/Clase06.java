e118377e4268ee9cafe91c19eb28ff7bfe9fc406
mike28gt <miguelalejandro28@gmail.com>
2022-08-31 23:09:15 -0600

    Commit inicial

---
+++ 2022-Clase06-SeccionA/src/clase06/Clase06.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase06;
+
+/**
+ *
+ * @author miguelcatalan
+ */
+public class Clase06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        boolean a = false;
+        boolean b = !a;
+        
+        System.out.println(b);
+        
+    }
+    
+}
