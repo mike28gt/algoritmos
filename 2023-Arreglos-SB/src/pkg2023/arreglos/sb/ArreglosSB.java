b412e602915ed016445236737d8fd5bb706d114c
mike28gt <miguelalejandro28@gmail.com>
2023-09-29 17:49:04 -0600

    Se agregan ejemplos de arreglos.

---
+++ 2023-Arreglos-SB/src/pkg2023/arreglos/sb/ArreglosSB.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2023.arreglos.sb;
+
+/**
+ *
+ * @author miguelcatalan
+ */
+public class ArreglosSB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Vectores
+        // Creación de vectores
+        // Asignación de datos al vector
+        // Recorrer un vector
+        
+        //Instrucción for mejorada
+        // for (tipo_dato variable : nombreVector) { }
+        
+        //Matrices
+        // Creación de arreglos bidimensionales
+        // Asignación de datos a las matrices
+        // Recorrer una matriz
+        
+        //Arrays
+        // Arrays.sort
+        // Arrays.fill
+        // Arrays.equals
+        // Arrays.binarySearch
+        
+        //ArrayList
+        // add
+        // clear
+        // contains
+        // get
+        // indexOf
+        // remove
+        // size
+            
+        
+    }
+    
+}
