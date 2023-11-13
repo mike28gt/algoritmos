dfa9e29afc20722beff2fffb6c8a3403434f7cc1
mike28gt <miguelalejandro28@gmail.com>
2023-11-12 22:19:00 -0600

    Se agregan ejemplos Algoritmos.

---
+++ 2023-HerenciaPolimorfismo-SA/src/pkg2023/herenciapolimorfismo/sa/Tigre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2023.herenciapolimorfismo.sa;
+
+/**
+ *
+ * @author miguelcatalan
+ */
+public class Tigre extends Carnivoro {
+    
+    public void cazar() {
+        System.out.println("El tigre sale a cazar...");
+    }
+}
